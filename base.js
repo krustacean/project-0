238f11021e59793708f6a9318f8ea4a2d50fda56
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-12 20:53:20 -0700

    refactored base.js a little bit

--- base.js
+++ base.js
@@ -1,32 +1,30 @@
 console.log('this is the right js file');
 
-
+var postHtml = '<div class="col-sm-8 col-sm-offset-2"><div class="thumbnail">'
+                + '<div class="caption"><button type="button" name="button" class="btn btn-xs pull-right delete">'
+                + '<span class="glyphicon glyphicon-remove"></span></button>'
+                + '<h3> '
 
 $(document).ready(function() {
 // add event listeners to page
 
 // delete post when the X is clicked
+  $('.delete').on('click', function(){
+    $(this).parents()[2].remove();
+  });
 
-
-// get input from the title and textarea
+  // get input from the title vals and textarea
   $('#submit-button').on('click',function(){
-     title = $('#new-post-title').eq(0).val();
-     post = $('#new-post').eq(0).val();
-     $('#all-posts').prepend('<div class="col-sm-8 col-sm-offset-2"><div class="thumbnail">'
-                             + '<div class="caption"><button type="button" name="button" class="btn btn-xs pull-right delete">'
-                             + '<span class="glyphicon glyphicon-remove"></span></button>'
-                             + '<h3> ' + title + '</h3>' + '<p>' + post + '</p></div></div></div>');
+     $title = $('#new-post-title').eq(0).val();
+     $post = $('#new-post').eq(0).val();
+     // prepend html into post list
+     $('#all-posts').prepend( postHtml + $title + '</h3>' + '<p>' + $post + '</p></div></div></div>');
+     //clear forms
      $('#new-post-title').eq(0).val('');
      $('#new-post').eq(0).val('');
+     //apply the event handler to the newly created element
      $('.delete').on('click', function(){
        $(this).parents()[2].remove();
-       console.log($(this));
      });
   });
-});
-// append html onto page
-
-$('.delete').on('click', function(){
-  $(this).parents()[2].remove();
-  console.log($(this));
 });
