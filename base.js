175dac685f7e298a96ce5d6aa62550958087c86a
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-12 18:25:48 -0700

    completed the base goals

--- base.js
+++ base.js
@@ -4,21 +4,29 @@ console.log('this is the right js file');
 
 $(document).ready(function() {
 // add event listeners to page
-  // $('#del').on('click', function(){
-  //   console.log(this.parent());
-  // });
+
+// delete post when the X is clicked
+
 
 // get input from the title and textarea
   $('#submit-button').on('click',function(){
      title = $('#new-post-title').eq(0).val();
      post = $('#new-post').eq(0).val();
      $('#all-posts').prepend('<div class="col-sm-8 col-sm-offset-2"><div class="thumbnail">'
-                             + '<div class="caption"><button type="button" name="button" class="btn btn-xs pull-right" id="del">'
+                             + '<div class="caption"><button type="button" name="button" class="btn btn-xs pull-right delete">'
                              + '<span class="glyphicon glyphicon-remove"></span></button>'
                              + '<h3> ' + title + '</h3>' + '<p>' + post + '</p></div></div></div>');
      $('#new-post-title').eq(0).val('');
      $('#new-post').eq(0).val('');
+     $('.delete').on('click', function(){
+       $(this).parents()[2].remove();
+       console.log($(this));
+     });
   });
 });
 // append html onto page
-// delete post when the X is clicked
+
+$('.delete').on('click', function(){
+  $(this).parents()[2].remove();
+  console.log($(this));
+});
