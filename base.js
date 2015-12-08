5a6d2ab9c9f4936c10f17ed648ebc7323dc02000
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-12-07 18:37:29 -0800

    adds parse to save basic posts

--- base.js
+++ base.js
@@ -1,10 +1,19 @@
-console.log('this is the right js file');
+Parse.initialize("N8XfwKZnBHwZRKSREAxaCTQM3gXbIGSUDQgMJ8jT", "rXF9Ekvm9KUoAT0A69qRp2NRlXZf1xN2aREhVrX9");
 
 var postHtml = '<div class="col-sm-8 col-sm-offset-2"><div class="thumbnail">'
-                + '<div class="caption"><button type="button" name="button" class="btn btn-xs pull-right delete">'
-                + '<span class="glyphicon glyphicon-remove"></span></button>'
-                + '<h3> '
+             + '<div class="caption"><button type="button" name="button" class="btn btn-xs pull-right delete">'
+             + '<span class="glyphicon glyphicon-remove"></span></button>'
+             + '<h3> '
 var D = new Date();
+
+var Post = Parse.Object.extend("post");
+var post = new Post();
+
+// post.set("title", 1337);
+// post.set("content", "Sean Plott");
+// gameScore.set("cheatMode", false);
+
+
 
 $(document).ready(function() {
 // add event listeners to page
@@ -17,10 +26,23 @@ $(document).ready(function() {
   // get input from the title vals and textarea
   $('#submit-button').on('click',function(){
      $title = $('#new-post-title').eq(0).val();
-     $post = $('#new-post').eq(0).val();
+     $content = $('#new-post').eq(0).val();
+
+     post.save({content: $content , title : $title}, {
+       success: function(post) {
+         console.log(post)
+        $('#all-posts').prepend( postHtml + $title + '</h3>' + '<p>' + $content + '</p>' + dHtml + '</div></div></div>');
+        var post = new Post();
+       },
+       error: function(post, error) {
+         // Execute any logic that should take place if the save fails.
+         // error is a Parse.Error with an error code and message.
+         alert('Failed to create new object, with error code: ' + error.message);
+       }
+     });
      dHtml = '<p><small><i>Written on ' + D.toLocaleDateString();
      // prepend html into post list
-     $('#all-posts').prepend( postHtml + $title + '</h3>' + '<p>' + $post + '</p>' + dHtml + '</div></div></div>');
+
      //clear forms
      $('#new-post-title').eq(0).val('');
      $('#new-post').eq(0).val('');
